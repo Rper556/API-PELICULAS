41f0938898d05655b62eeb7b9ad22fb8b26a4b66
Rper556 <reaperodin556@gmail.com>
2026-05-29 19:04:41 -0700

    Fix database local and render

--- database.js
+++ database.js
@@ -1,14 +1,22 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-});
+const sequelize = process.env.DATABASE_URL
+
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
+
+  : new Sequelize({
+      dialect: 'sqlite',
+      storage: './database.sqlite',
+      logging: false
+    });
 
 export default sequelize;
