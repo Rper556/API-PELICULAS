99ae07e840cd209166d71757d25fb626fe1945e0
Rper556 <reaperodin556@gmail.com>
2026-05-29 18:24:27 -0700

    API funcionando localmente

--- database.js
+++ database.js
@@ -1,14 +1,9 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+  logging: false
 });
 
 export default sequelize;
